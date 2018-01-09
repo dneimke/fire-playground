385d6602606bbf47ae8cb844a408ecbc111cc010
Darren Neimke <darren.neimke@live.com>
2018-01-09 18:33:25 +1030

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDxOClCtrI2hibiH0ewvRUAtG6zWtTX37s",
+    authDomain: "ngrx-fire-fed89.firebaseapp.com",
+    databaseURL: "https://ngrx-fire-fed89.firebaseio.com",
+    projectId: "ngrx-fire-fed89",
+    storageBucket: "ngrx-fire-fed89.appspot.com",
+    messagingSenderId: "92663924686"
+  }
 };
